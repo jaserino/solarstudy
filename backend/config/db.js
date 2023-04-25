79ced72b755c9a5319422fbe9990455ebad65a52
jaserino <jaserino1@gmail.com>
2023-04-25 12:36:43 -0400

    mongoose 7 deprication change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // prepare for mongoose 7 deprication strictquery change
+    mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
