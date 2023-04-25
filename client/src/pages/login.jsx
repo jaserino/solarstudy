6895ba710c9c5c79d12f2a698431ff5329f7dafd
jaserino <jaserino1@gmail.com>
2023-04-25 12:23:24 -0400

    frontend setup

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,88 @@
+import loginStyles from '../../styles/Login.module.css';
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// useSelector allows us to select anything in the global state ex.. user, message etc.
+// useDispatch allows us to dispatch actions like calling register func
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../features/auth/authSlice';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
+  };
+
+  return (
+    <>
+      <section className={loginStyles.container}>
+        <div className={loginStyles.main}>
+          <h1 className={loginStyles.header}>Welcome Back</h1>
+          <div className={loginStyles.loginbox}>
+            <form onSubmit={onSubmit}>
+              <h3 className={loginStyles.loginheader}>Login</h3>
+              <div className={loginStyles.userbox}>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                  required
+                />
+                <label>email</label>
+              </div>
+              <div className={loginStyles.userbox}>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                  required
+                />
+                <label>password</label>
+              </div>
+              <button className={loginStyles.button}>ENGAGE</button>
+            </form>
+            <div className={loginStyles.notregistered}>
+              <div className={loginStyles.notResteredClick}>
+                Not Registered?
+              </div>
+              <a href="/signup">Sign up here!</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/signup.jsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import signupStyles from '../../styles/Signup.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// useSelector allows us to select anything in the global state ex.. user, message etc.
+// useDispatch allows us to dispatch actions like calling register func
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../features/auth/authSlice.js';
+
+const signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    cpassword: '',
+  });
+
+  const { name, email, password, cpassword } = formData;
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const { user, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    // Redirect when logged in
+    if (isSuccess && user) {
+      router.push('/');
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, user, message, router, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password != cpassword) {
+      toast.error('Passwords do not match', {
+        position: toast.POSITION.TOP_CENTER,
+        theme: 'dark',
+      });
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
+    }
+  };
+
+  return (
+    <>
+      <section className={signupStyles.container}>
+        <div className={signupStyles.main}>
+          <h1 className={signupStyles.header}>
+            Elevate your study habits today.
+          </h1>
+          <div className={signupStyles.signupbox}>
+            <form onSubmit={onSubmit}>
+              <h3 className={signupStyles.signupboxheader}>Register Now</h3>
+              <div className={signupStyles.userbox}>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={onChange}
+                  required
+                />
+                <label>username</label>
+              </div>
+              <div className={signupStyles.userbox}>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={onChange}
+                  required
+                />
+                <label>email</label>
+              </div>
+              <div className={signupStyles.userbox}>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={onChange}
+                  required
+                />
+                <label>password</label>
+              </div>
+              <div className={signupStyles.userbox}>
+                <input
+                  type="password"
+                  id="cpassword"
+                  name="cpassword"
+                  value={cpassword}
+                  onChange={onChange}
+                  required
+                />
+                <label>confirm password</label>
+              </div>
+              <button className={signupStyles.button}>ENGAGE</button>
+            </form>
+            <div className={signupStyles.alreadyRegistered}>
+              <a href="/login">Have an account? Login here.</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default signup;
