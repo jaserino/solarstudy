2c223ddb365567d5b370b7c42a60d41cdfa96897
jaserino <jaserino1@gmail.com>
2024-03-26 20:17:52 -0400

    redux

--- client/src/app/activeCard/ActiveCard.js
+++ client/src/app/activeCard/ActiveCard.js
@@ -1,7 +1,8 @@
 const initalState = {
   card: ['YouTube', 'Notes', 'Spotify', 'Timer'],
 };
-
+// TODO
+// Implement reducers for icon color change
 export const activeCard = createSlice({
   name: 'card',
   initalState,
