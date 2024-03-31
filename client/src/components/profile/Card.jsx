a39f34f33ceedb71914ff59fe0d018479dec8f71
jaserino <jaserino1@gmail.com>
2024-03-30 21:44:25 -0400

    update

--- client/src/components/profile/Card.jsx
+++ client/src/components/profile/Card.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cardStyles from '../../../styles/componentsCSS/Card.module.css';
 import Draggable from 'react-draggable';
 import { IoClose } from 'react-icons/io5';
 
 import { useDispatch } from 'react-redux';
-import { minimizeCard } from '../../app/activeCard/ActiveCard';
+
+// TODO
+/* get rid of dynamic card component
+   each card needs to be unique
+   each card should be made draggable on its own
+   make individual cards not dynamic card 
+*/
 
 const Card = ({ content, label }) => {
-  const nodeRef = React.useRef(null);
   const dispatch = useDispatch();
 
   const minimizeIcon = () => {
@@ -15,8 +20,8 @@ const Card = ({ content, label }) => {
   };
 
   return (
-    <Draggable nodeRef={nodeRef}>
-      <div className={cardStyles.card} ref={nodeRef}>
+    <Draggable bounds="body">
+      <div className={cardStyles.card}>
         <div className={cardStyles.minimize}>
           <IoClose onClick={minimizeIcon} />
         </div>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,7 +7,6 @@ import Notes from '../components/profile/NotesContent';
 import Timer from '../components/profile/TimerContent';
 import Spotify from '../components/profile/SpotifyContent';
 import IconMenu from '../components/profile/IconMenu';
-
 import { useSelector, useDispatch } from 'react-redux';
 import { removeCard, addCard } from '../app/activeCard/ActiveCard';
 
@@ -40,18 +39,11 @@ const Profile = () => {
         <IconMenu onIconClick={handleIconClick} />
       </div>
       <div className={profileStyles.container}>
-        <div className={profileStyles.cardGrid}>
-          {activeCards.labels.map((label, i) => (
-            <div
-              key={label}
-              className={`${profileStyles.gridItem} ${
-                profileStyles[`gridItem${i}`]
-              }`}
-            >
-              <Card label={label} content={getComponentContent(label)} />
-            </div>
-          ))}
-        </div>
+        {activeCards.labels.map((label) => (
+          <div>
+            <Card label={label} content={getComponentContent(label)} />
+          </div>
+        ))}
       </div>
     </>
   );
